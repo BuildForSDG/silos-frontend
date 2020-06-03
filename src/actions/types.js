ee8c8a7ba6a27487520fb24c05e3e1eb4122ee8e
wincodes <otokinaodafe@gmail.com>
2020-06-03 20:26:51 +0100

    feat: implement login functionality, redux state management and finish up signup process

---
+++ src/actions/types.js
@@ -0,0 +1,6 @@
+export const GET_ERRORS = 'GET_ERRORS ';
+export const SET_CURRENT_USER = 'SET_CURRENT_USER';
+export const NOTIFICATION = 'NOTIFICATION';
+export const GET_POSTS = 'GET_POSTS';
+export const ADD_POST = 'ADD_POST';
+export const URL = 'https://silos-app.herokuapp.com/api/v1'
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,9 +2,8 @@ import React, { Component } from 'react'
 import { Link } from "react-router-dom";
 import PropTypes from 'prop-types' //ES6
 import logo from '../../images/logo.png';
-
-
-
+import { logoutUser } from "../../actions/authActions";
+import { connect } from 'react-redux';
 import './navbar.css'
 
 class Navbar extends Component {
@@ -15,10 +14,45 @@ class Navbar extends Component {
         }
     }
 
+    onLogoutClick(e) {
+        e.preventDefault()
+        this.props.logoutUser()
+    }
+
     render() {
+        const { isAuthenticated, user } = this.props.auth
+
+        let authLinks
+
+        if (isAuthenticated) {
+            authLinks = (
+                <ul className="navbar-nav nav-item-right">
+                    <li className="nav-link">
+                        {user.email}
+                    </li>
+
+                    <li
+                        onClick={this.onLogoutClick.bind(this)}
+                        className='nav-link'
+                        style={{ cursor: 'pointer' }}
+                    >
+                        Log Out
+					</li>
+                </ul>)
+        } else {
+            authLinks = (
+                <ul className="navbar-nav nav-item-right">
+                    <li className="nav-item">
+                        <Link to="/user/login" className="nav-link" id="login">log in</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/user/register" className="nav-link" id="register">Register</Link>
+                    </li>
+                </ul>)
+        }
 
         return (
-            <nav  className=" navbar navbar-expand-lg navbar-light">
+            <nav className=" navbar navbar-expand-lg navbar-light">
                 <Link to="/" className="navbar-brand"><img src={logo} alt="silos-logo"></img></Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -38,17 +72,7 @@ class Navbar extends Component {
                             <Link to="/support" className="nav-link">Support</Link>
                         </li>
 
-                        <ul className="navbar-nav nav-item-right">
-                            <li className="nav-item">
-                                <Link to="/user/login" className="nav-link" id="login">log in</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/user/register" className="nav-link" id="register">Register</Link>
-                            </li>
-
-                        </ul>
-
-
+                        {authLinks}
                     </ul>
                 </div>
             </nav>
@@ -57,7 +81,11 @@ class Navbar extends Component {
 }
 
 Navbar.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+}
 
-};
-
-export default Navbar
+const mapStateToProps = state => ({
+    auth: state.auth
+})
+export default connect(mapStateToProps, { logoutUser })(Navbar)
